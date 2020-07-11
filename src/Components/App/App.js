06f5a693c7c22c7cd5c6297f37f823a48f081976
Rob Koebke <robert.koebke@gmail.com>
2020-07-11 19:17:26 -0400

    Complete step #28 of Jammming.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
 
-export class App extends React.Component {
+class App extends React.Component {
 
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          { /* Add a SearchBar component */ }
+            <SearchBar />
           <div className="App-playlist">
-            { /* Add a SearchResults component */ }
-            { /* Add a Playlist component */ }
+            <SearchResults />
+            <Playlist />
           </div>
         </div>
       </div>
