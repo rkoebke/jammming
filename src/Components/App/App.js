028deb512d33b48a2bc973918714efad53337bfd
Rob Koebke <robert.koebke@gmail.com>
2020-07-13 13:54:17 -0400

    Complete step 39 in Jammming project.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,16 +11,43 @@ class App extends React.Component {
     this.state = {
       searchResults: [
         {
-          name: '',
-          artist: '',
-          album: '',
-          id: ''
+          name: 'name1',
+          artist: 'artist1',
+          album: 'album1',
+          id: '1'
         },
         {
-          name: '',
-          artist: '',
-          album: '',
-          id: ''
+          name: 'name2',
+          artist: 'artist2',
+          album: 'album2',
+          id: '2'
+        },
+        {
+          name: 'name3',
+          artist: 'artist3',
+          album: 'album3',
+          id: '3'
+        }
+      ],
+      playlistName: 'playlistName',
+      playlistTracks: [
+        {
+          name: 'playlistName1',
+          artist: 'playlistArtist1',
+          album: 'playlistAlbum1',
+          id: '4'
+        },
+        {
+          name: 'playlistName2',
+          artist: 'playlistArtist2',
+          album: 'playlistAlbum2',
+          id: '5'
+        },
+        {
+          name: 'playlistName3',
+          artist: 'playlistArtist3',
+          album: 'playlistAlbum3',
+          id: '6'
         }
       ]
     };
@@ -36,7 +63,7 @@ class App extends React.Component {
             <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './TrackList.css';
 import { Track } from '../Track/Track';
 
-export class TrackList extends React.Components {
+export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
